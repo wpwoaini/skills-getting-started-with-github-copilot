81ae6c20c91ef729ec15044882c03aa5bd99939c
wpwoaini <128487021+wpwoaini@users.noreply.github.com>
2025-05-26 14:57:36 +0000

    添加测试配置文件，创建基本的 hello world 测试函数

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ A super simple FastAPI application that allows students to view and sign up
 for extracurricular activities at Mergington High School.
 """
 
+
 from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
